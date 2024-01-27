c5c38341d8ede144faef56669982ce2e3845075e
sha5b <ned.tabulov@gmail.com>
2024-01-27 16:23:49 +0100

    Update

--- src/lib/components/CategoryDistributor.svelte
+++ src/lib/components/CategoryDistributor.svelte
@@ -7,15 +7,23 @@
 	import WorkDistributor from './WorkDistributor.svelte';
 
 	export let categories = [];
-	export let size = new Vector3(500, 250, 250);
+	export let size = new Vector3(500, 500, 500);
+
+
+
+
 
 	// Define the range for each axis and the step size.
-	const range = new Vector3(2000, 2000, 2000);
-	const step = 250; // This should be the minimum size or a common factor of sizes.
+	const range = new Vector3(3000, 3000, 3000);
+
+
+
 
 	// Helper function to generate a random position on the grid.
-	function getRandomGridPosition(range, step, size) {
-		// ... calculations for x, y, z
+	function getRandomGridPosition(range, size) {
+		// Calculate the step size based on the size of the current box.
+		const step = Math.max(size.x, size.y, size.z);
+
 		return new Vector3(
 			Math.floor(Math.random() * ((range.x - size.x) / step)) * step - (range.x - size.x) / 2,
 			Math.floor(Math.random() * ((range.y - size.y) / step)) * step - (range.y - size.y) / 2,
@@ -23,14 +31,43 @@
 		);
 	}
 
+	
+
+
+	function isOverlapping(position, size) {
+		for (let [_, otherPosition] of categoryPositions) {
+			if (
+				position.x < otherPosition.x + size.x &&
+				position.x + size.x > otherPosition.x &&
+				position.y < otherPosition.y + size.y &&
+				position.y + size.y > otherPosition.y &&
+				position.z < otherPosition.z + size.z &&
+				position.z + size.z > otherPosition.z
+			) {
+				// There is an overlap
+				return true;
+			}
+		}
+		// No overlap with any other boxes
+		return false;
+	}
+
+	
 	// Generate random positions for each category and store them in a map.
-	const categoryPositions = new Map(
-		categories.map((category) => [category.id, getRandomGridPosition(range, step, size)])
-	);
+	const categoryPositions = new Map();
+	categories.forEach((category) => {
+		// Assume each category has a 'size' property with x, y, and z dimensions.
+		let pos = getRandomGridPosition(range, size);
+		while (isOverlapping(pos, size)) {
+			// If overlap detected, generate a new position.
+			pos = getRandomGridPosition(range, size);
+		}
+		categoryPositions.set(category.id, pos);
+	});
 </script>
 
 {#each categories as category (category.id)}
 	<CategoryBox position={categoryPositions.get(category.id)} {size}>
-		<WorkDistributor/>
+		<WorkDistributor />
 	</CategoryBox>
 {/each}
