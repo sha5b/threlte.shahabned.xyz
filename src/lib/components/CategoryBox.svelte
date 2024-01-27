589f59f9cb9e96137c410759572867b238d81abc
sha5b <ned.tabulov@gmail.com>
2024-01-27 16:54:06 +0100

    Refactoring Start

--- src/lib/components/CategoryBox.svelte
+++ src/lib/components/CategoryBox.svelte
@@ -5,8 +5,8 @@
 	import { MeshLineGeometry, MeshLineMaterial } from '@threlte/extras';
 
 	export let position = new Vector3(0, 0, 0);
-	export let size = new Vector3(250, 250, 250);
-	export let cellSize = 50;
+	export let size = new Vector3(500, 500, 500);
+	export let cellSize = 250;
 	export let width = 2;
 	export let color = 'red';
 
