e596512924b1aed4accc97f79746681f69c7b03a
sha5b <ned.tabulov@gmail.com>
2024-02-03 10:48:32 +0100

    Update Scene.svelte

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -1,11 +1,11 @@
 <script>
 	//@ts-nocheck
+	
 	import { T } from '@threlte/core';
 	import { OrbitControls } from '@threlte/extras';
 	import { Vector3 } from 'three';
 	import CategoryDistributor from '$lib/components/CategoryDistributor.svelte';
 	import { writable } from 'svelte/store';
-	import { tweened } from 'svelte/motion';
 
 	export let data; //Pasted data from the Database
 
@@ -14,37 +14,24 @@
 	let cameraFOV = 25;
 	let cameraRotation = writable([0, 0, 0]);
 
-  // Define your fog color and density
-  let fogColor = 'black'; // white fog
-  let density = 0.25; // The density of the fog. Default is 0.00025.
+
 	
 	// Handle Mouse Events
 
-	function onBoxClick(event) {
-		const { position, size, rotation } = event.detail; // Get the position and size of the category box
+function onBoxClick(event) {
+	const { position, size } = event.detail;
+	const extraSpaceFactor = 1.2;
+	const direction = new Vector3(...$cameraPosition).sub(position).normalize();
+	const adjustedDistance = (size.y / Math.tan(cameraFOV * Math.PI / 360)) * extraSpaceFactor;
+	const newCameraPosition = direction.multiplyScalar(-adjustedDistance).add(position);
+	newCameraPosition.x += (Math.random() - 0.5) * 6000;
+	newCameraPosition.y += (Math.random() - 0.5) * 6000;
+	newCameraPosition.z += (Math.random() - 0.5) * 6000;
 
-		// Calculate the distance required to fit the box in view
-		const distanceToFitBox = size.y / 2 / Math.tan((cameraFOV * Math.PI) / 180 / 2);
-		const extraSpaceFactor = 1.2; // Adjust to give more or less space around the box
-		const adjustedDistance = distanceToFitBox * extraSpaceFactor;
-
-		// Calculate the new camera position, maintaining the direction from the camera to the box's center
-		const direction = new Vector3(...$cameraPosition).sub(new Vector3(...position)).normalize();
-		const newCameraPosition = direction
-			.multiplyScalar(-adjustedDistance)
-			.add(new Vector3(...position));
-
-		// Apply a random offset to the new camera position
-		const randomOffset = () => Math.random() * 6000 - 3000; // Adjust the range as needed
-		newCameraPosition.x += randomOffset();
-		newCameraPosition.y += randomOffset();
-		newCameraPosition.z += randomOffset();
-
-		cameraTarget.set(position);
-		cameraPosition.set([newCameraPosition.x, newCameraPosition.y, newCameraPosition.z]);
-		console.log('rotation', rotation.map((r) => (r * 180) / Math.PI).join(', '));
-		cameraRotation.set(rotation);
-	}
+	cameraTarget.set(position);
+	cameraPosition.set([newCameraPosition.x, newCameraPosition.y, newCameraPosition.z]);
+	cameraRotation.set(event.detail.rotation);
+}
 </script>
 
 <T.PerspectiveCamera bind:position={$cameraPosition} makeDefault fov={cameraFOV} far={500000} >
@@ -56,7 +43,6 @@
 		autoRotateSpeed={0.25}
 	/>
 </T.PerspectiveCamera>
-<T.FogExp2 color={fogColor} density={density} />
 <T.DirectionalLight intensity={0.8} position.x={5} position.y={10} />
 <T.AmbientLight intensity={0.2} />
 
