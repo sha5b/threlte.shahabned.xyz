fda77f31cdbf06f3bed5c021dd09f6c54920b533
sha5b <ned.tabulov@gmail.com>
2024-02-12 12:31:13 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,26 +2,34 @@
 	//@ts-nocheck
 	import App from '$lib/components/App.svelte';
 	export let data;
-	
+
+	$: selectedCategoryId = null;
+	let selectedWorkId = null;
 
 	function handleBoxClick(event) {
-    const categoryId = event.detail.id;
-	console.log("+page recvied", categoryId)
-    // Handle category ID as needed
-  }
+		const selectedCategoryId = event.detail.id;
+		console.log('+page recvied', selectedCategoryId);
+	}
 
-  function handleWorkClick(event) {
-    const workId = event.detail.id;
-	console.log("+page recvied", workId)
-    // Handle work ID as needed
-  }
-
+	function handleWorkClick(event) {
+		selectedWorkId = event.detail.id;
+		console.log('+page recvied', selectedWorkId);
+		// Handle work ID as needed
+	}
 </script>
 
+<nav>
+	<h1>shahab nedaei</h1>
+	{#each data.categories as category}
+		<button>{category.title}</button>
+	{/each}
+	<h1>{selectedCategoryId}</h1>
+	<h1>{selectedWorkId}</h1>
+</nav>
 <flex>
-	<div>
-		<App {data}  on:boxclick={handleBoxClick} on:workclick={handleWorkClick}/>
-	</div>
+	<scene>
+		<App {data} on:boxclick={handleBoxClick} on:workclick={handleWorkClick} />
+	</scene>
 </flex>
 
 <style>
@@ -29,16 +37,24 @@
 		margin: 0px;
 	}
 	flex {
+
 		padding: 25px;
 		display: flex;
 		align-items: stretch;
 		justify-content: center;
-		flex-direction: column;
+
 	}
-	div {
+	scene {
 		flex-grow: 1;
 		min-height: 95vh;
 		background: rgb(13, 19, 32);
 		background: linear-gradient(180deg, rgb(229, 65, 80) 0%, rgb(199, 57, 69) 100%);
+	}
+
+	nav {
+		background: rgb(255, 255, 255);
+		position: absolute;
+		right: 0px;
+		bottom: 0px;
 	}
 </style>
