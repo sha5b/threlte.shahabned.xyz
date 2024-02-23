661df5a1454efa7de7835c05ae825df730f9e021
sha5b <ned.tabulov@gmail.com>
2024-02-23 19:08:11 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,13 +32,8 @@
 	}
 	function setWorkId(id) {
 		if (selectedWorkId !== id) {
-			closeAllModals(); // Close any existing modals when switching works
 			currentId = id;
 			selectedWorkId = id;
-			const work = data.works.find((w) => w.id === id);
-			if (work && work.synopsis) {
-				openModal(work.synopsis);
-			}
 		}
 	}
 
@@ -66,40 +61,7 @@
 		const date = new Date(dateString);
 		return date.toLocaleDateString(undefined, options);
 	}
-
-	let modals = [];
-
-	function openModal(content) {
-		const maxWidthPercentage = 80; // Maximum width of modal in percentage of viewport width
-		const maxHeightPercentage = 80; // Maximum height of modal in percentage of viewport height
-		const modalWidth = 20; // Approximate width of modal in percentage of viewport width
-		const modalHeight = 20; // Approximate height of modal in percentage of viewport height
-
-		const modal = {
-			id: Math.random().toString(36).substr(2, 9),
-			content,
-			x: Math.random() * (maxWidthPercentage - modalWidth), // random X position within viewport bounds
-			y: Math.random() * (maxHeightPercentage - modalHeight) // random Y position within viewport bounds
-		};
-		modals = [...modals, modal];
-	}
-
-	function closeModal(id) {
-		modals = modals.filter((modal) => modal.id !== id);
-	}
-	function closeAllModals() {
-		modals = [];
-	}
 </script>
-
-{#each modals as modal (modal.id)}
-	<div class="modal" style="left: {modal.x}%; top: {modal.y}%;">
-		<button class="close-btn" on:click={() => closeModal(modal.id)}>Close</button>
-		<div class="content">
-			{@html modal.content}
-		</div>
-	</div>
-{/each}
 
 <Navigation
 	{selectedCategoryId}
@@ -138,25 +100,5 @@
 		min-height: 100vh;
 		background: rgb(13, 19, 32);
 		background: radial-gradient(circle at center, rgb(206, 37, 88) 0%, rgb(16, 129, 110) 100%);
-	}
-
-	.modal {
-		position: absolute;
-		width: 500px;
-		max-height: 100%;
-		background-color: white;
-		border: 1px solid #ccc;
-		box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
-		padding: 10px;
-		box-sizing: border-box;
-		z-index: 10;
-	}
-	.close-btn {
-		position: absolute;
-		top: 0;
-		right: 0;
-	}
-	.content {
-		/* Styles for the content area */
 	}
 </style>
